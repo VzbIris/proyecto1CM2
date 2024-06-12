7c80b78b0d45a0f77cbdf372ee04a267ddc9b6c0
VzbIris <sirivb1403@gmail.com>
2024-06-11 18:51:25 -0600

    Codigo que realiza la suma

--- mainp3.cpp
+++ mainp3.cpp
@@ -1,5 +1,19 @@
 #include <stdio.h>
-
+float suma();
 int main(){
-	printf("a");
+
+float i,j;
+int Resultado;
+printf(" Ingresa el valor del primer numero = ");
+scanf("%f",&i);
+printf(" Ingresa el valor del segundo numero = ");
+scanf("%f",&j);
+Resultado= i+ j;
+float suma();
+}
+float suma(){
+int Resultado;
+float i,j;
+Resultado= i+ j;
+return Resultado;
 }
